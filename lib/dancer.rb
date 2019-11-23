906635c99ab6a9b96798047fa2f3988c4a07051e
Kizito Njoku <nkizito57@gmail.com>
2019-11-23 01:08:25 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,14 +3,14 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  # include Dance
+  include Dance
  
-  # attr_accessor :name
+  attr_accessor :name
  
-  # def initialize(name)
-  #   @name = name
-  # end
-  # extend MetaDancing
+  def initialize(name)
+    @name = name
+  end
+  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
